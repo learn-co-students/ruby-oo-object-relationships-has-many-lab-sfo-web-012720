17df3b56b830a5edbdfe4b0e472038bb27fcdbbd
amnyrrd <amnyrrd@gmail.com>
2020-02-01 12:12:48 -0800

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,13 +21,6 @@ class Artist
         Song.all.select {|song| song.artist == self}
     end
 
-    def artist_name
-        if song.artist == nil
-            return nil
-        else
-            Song.all.select {|song| song.artist == self}
-        end
-    end
 
     def self.song_count
         Song.all.length
